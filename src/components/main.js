7298e14b5899afce64a4ab150a3f0d6c7731bc22
balirajani1408 <balirajani1408@gmail.com>
2023-08-14 23:46:05 -0700

    check

--- src/components/main.js
+++ src/components/main.js
@@ -5,7 +5,9 @@ export const Main = () =>{
     return(
         <div className="main">
             <h2>Main content</h2>
+           
             <p>Attendance</p>
+             <p>Attendance</p>
         </div>
     )
 }
